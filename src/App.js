8ba457a146190d8c901f1322f844b77679d5c978
Ben <ben.dalton@madetech.com>
2020-11-16 20:22:17 +0000

    convert exchange rate data to array of options in currencyOptions state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
 const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions);
+
   useEffect(() =>  {
-    fetch(BASE_URL).then(res => res.json()).then(data => console.log(data))
+    fetch(BASE_URL).then(res => res.json()).then(data => {
+      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+    })
   }, [])
 
   return (
